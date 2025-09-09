7beb40748d6891a446db408ab5258ceb0964f57a
Znex <znexnon@gmail.com>
2025-09-09 23:34:08 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -2,18 +2,20 @@ return {
     Camera = { DefaultFOV = 70 },
     Visuals = { Saturation = 100, Brightness = 1 },
 
-    ESP = {
-        EnabledBox     = false,
-        ShowName       = false,
-        ShowDistance   = false,
-        ShowTracers    = false,
-        TeamCheck      = true,
-        TracerThickness= 1,
-        TracerOrigin   = "Bottom",
-        MinBoxW = 6, MinBoxH = 6, Padding = 2,
-        BoxColor    = Color3.fromRGB(255,165,0),
-        TracerColor = Color3.fromRGB(255,165,0)
-    },
+    -- In Config.lua, în tabela ESP:
+ESP = {
+    EnabledBox     = false,
+    ShowName       = false,
+    ShowDistance   = false,
+    ShowTracers    = false,
+    -- TeamCheck    = true,   <-- SCOATE linia asta!
+    TracerThickness= 1,
+    TracerOrigin   = "Bottom",
+    MinBoxW = 6, MinBoxH = 6, Padding = 2,
+    BoxColor    = Color3.fromRGB(255,165,0),
+    TracerColor = Color3.fromRGB(255,165,0)
+},
+
 
     HitboxExtender = {
         Enabled  = false,
