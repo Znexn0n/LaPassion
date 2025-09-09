ff63bb831f298e53818e9d70d4230905a6548ab4
Znex <znexnon@gmail.com>
2025-09-09 03:44:39 +0200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,25 @@
+return {
+    Camera = { DefaultFOV = 70 },
+    Visuals = { Saturation = 100, Brightness = 1 },
+
+    ESP = {
+        EnabledBox     = false,
+        ShowName       = false,
+        ShowDistance   = false,
+        ShowTracers    = false,
+        TeamCheck      = true,
+        TracerThickness= 1,
+        TracerOrigin   = "Bottom",
+        MinBoxW = 6, MinBoxH = 6, Padding = 2,
+        BoxColor    = Color3.fromRGB(255,165,0),
+        TracerColor = Color3.fromRGB(255,165,0)
+    },
+
+    HitboxExtender = {
+        Enabled  = false,
+        BodySize = Vector3.new(13,13,13),
+        HeadSize = Vector3.new(10,10,10)
+    },
+
+    Watermark = { Enabled = false }
+}
